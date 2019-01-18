f40796fd57ff172a1c54232fa939b5ec069b6890
Eduardo Díaz Cortés <ediaz@dcc8090.cl>
2019-01-18 14:39:12 -0300

    solves 12

--- clojure/11-20/p12.clj
+++ clojure/11-20/p12.clj
@@ -1,1 +1,17 @@
-(ns p12)
+
+(defn divisors [n]
+      (filter #(zero? (rem n %)) (range 1 (inc (Math/sqrt n)))))
+
+
+; tn[n] = n * (n+1)/2
+(defn triangle-num [n]
+      (/ (* n (inc n)) 2))
+
+(def triangles
+      (map triangle-num (iterate inc 1)))
+
+(defn num-div [n]
+      (* 2 (count (divisors n))))
+
+
+(println (first (drop-while #(< (num-div %) 500) triangles)))
