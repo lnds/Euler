184df0e7a471ccce27a184c2333de058d95e1ed4
Eduardo Díaz Cortés <ediaz@dcc8090.cl>
2019-01-23 13:01:12 -0300

    solves problem 22

---
+++ clojure/21-30/p22.clj
@@ -0,0 +1,10 @@
+(require '[clojure.string :as str])
+
+(defn strip-quotes [s] (subs s 1 (dec (count s))))
+
+(defn name-value [[pos name]] (* (inc pos) (reduce + (map #(inc (- (int %) (int \A))) name))))
+
+
+
+(let [names (sort (map strip-quotes (str/split  (slurp "names.txt") #","))) ]
+    (println (reduce + (map name-value (map-indexed vector names))))  )
