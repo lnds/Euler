49c77c31c6de00eb8ceefac7756358b35b4d0256
Eduardo Díaz Cortés <ediaz@dcc8090.cl>
2019-01-29 15:41:04 -0300

    refactor 8,9 & 10

--- clojure/p9.clj
+++ clojure/p9.clj
@@ -1,5 +1,6 @@
+(ns p9)
 
-(let [[[a b c]] 
+(let [[[a b c]]
 	  (for [a (range 1 250) b (range 251 500) c (range 1 500) 
 		:when (and (= 1000 (+ a b c)) (= (+ (* a a) (* b b)) (* c c)))] [a b c])]
 	(println (* a b c)))
