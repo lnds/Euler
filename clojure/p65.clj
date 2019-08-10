1c154592bfec4f4695ba6c60bf8dfeaa20dbfafe
Eduardo Díaz <ediaz@dcc8090.cl>
2019-08-10 11:45:33 -0400

    add some fixes

--- clojure/p65.clj
+++ clojure/p65.clj
@@ -1,1 +1,10 @@
-(ns p65)
+(ns p65 (:use [digits]))
+
+(defn calc-n-convergent [limit]
+      (loop [d 1N, n 2N, i 2N]
+            (if (> i limit)
+              n
+              (let [c (if (zero? (mod i 3)) (* 2 (/ i 3)) 1)]
+                   (recur n (+ d  (* c n)) (inc i))))))
+
+(println (reduce + (num-to-digits (calc-n-convergent 100))))
